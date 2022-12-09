71d750e8c4c896c350b7ca73e4db9188277bcf33
theremist <81764337+theremist@users.noreply.github.com>
2022-12-09 11:09:26 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Meta-->
+        <?php include_once 'helpers/help_meta.php';?>
+        
+        <title>LM4 - Luis Filipe Gomes</title>
+
+        <!-- CSS Fonts-->
+        <?php include_once 'helpers/help_css.php';?>
+        
+    </head>
+    <body id="page-top">
+
+        <!-- Navigation-->
+        <?php include_once 'components/cp_navigation.php';?>
+
+        <!-- Masthead-->
+        <?php include_once 'components/cp_header.php';?>
+        
+        <!-- Articles-->
+        <?php include_once 'components/cp_articles.php';?>
+
+        <!-- Footer-->
+        <?php include_once 'components/cp_footer.php';?>
+
+         <!-- JS-->       
+        <?php include_once 'helpers/help_js.php';?>
+
+    </body>
+</html>
